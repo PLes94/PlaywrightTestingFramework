fe6d510bb5743f3a2fb857a25450b052d5184e4b
Piotr L <suspicioushero@gmail.com>
2023-10-10 22:19:53 +0200

    Add new TestExample class, add BrowserContext class to main() method

--- src/main/java/org/example/PlaywrightTest.java
+++ src/main/java/org/example/PlaywrightTest.java
@@ -1,19 +1,16 @@
 package org.example;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 
 public class PlaywrightTest {
 
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions()
-                            .setHeadless(false)
-                            .setSlowMo(50));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false)
+                    .setSlowMo(50));
             Page page = browser.newPage();
+            BrowserContext context = browser.newContext();
             page.navigate("http://playwright.dev");
             System.out.println(page.title());
         }
